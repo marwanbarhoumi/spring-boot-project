c89607afa488364cf2c523492ce1370c6b142195
Ghofrane <you@example.com>
2024-12-10 10:45:24 +0000

    Initial commit for examen-devops

---
+++ README.md
@@ -0,0 +1,2 @@
+# examen-devops
+# examen-devops
---
+++ src/test/java/com/examen/AppTest.java
@@ -0,0 +1,12 @@
+package com.examen;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);  // Ce test doit passer
+    }
+}
